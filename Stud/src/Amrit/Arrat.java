4dc393f5043529be6a7854cdb5c077abc49a287b
Amritpal4643 <amritpalsingh3027@gmail.com>
2019-01-30 19:26:49 -0500

    Array handling

---
+++ Stud/src/Amrit/Arrat.java
@@ -0,0 +1,29 @@
+package Amrit;
+
+public class Arrat
+{
+    public static void main(String args[])
+    {
+        int a[];
+        int b[] = new int[]{1, 2, 3, 4, 5};
+        int c[] = {1, 2, 3, 4, 5};
+        int d[] = new int[5];
+        a = new int[3];
+        a[0] = 100;
+        a[1] = 200;
+        a[2] = 300;
+        System.out.println(a[0]);
+        int[] x1,x2,x3;
+        x2 = new int[5];
+        int y1[],y2;
+        int[] z1,z2,z3[];
+        //z3 = new int[3][3];
+        z3  = new int[5];
+
+
+    }
+}
+
+
+
+
